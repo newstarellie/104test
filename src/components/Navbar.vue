c0961e31b5cdbb48999709804b818f3c59fbb45f
newstarellie <newstarellie@gmail.com>
2023-07-11 10:00:50 +0800

    set router basic setting

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,10 +2,10 @@
   <nav>
     <ul>
       <li>
-        <router-link to="/">Home</router-link>
+        <router-link to="/">課程列表</router-link>
       </li>
       <li>
-        <router-link to="/contact">Contact</router-link>
+        <router-link to="/cartPage">購物車</router-link>
       </li>
     </ul>
   </nav>
