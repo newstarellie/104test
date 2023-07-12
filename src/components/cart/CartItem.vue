6aea36d4c04c5b3b42fd10ce400120643639e2b7
newstarellie <newstarellie@gmail.com>
2023-07-12 11:11:53 +0800

    fix  error  Unexpected mutation of "item" prop

--- src/components/cart/CartItem.vue
+++ src/components/cart/CartItem.vue
@@ -1,16 +1,16 @@
 <template>
   <div :class="['flex items-center border p-4 mb-4', { 'hidden sm:block': !isMobile }]">
     <input type="checkbox"
-      v-model="item.selected"
-      :value="item.id"
+      v-model="itemCopy.selected"
+      :value="itemCopy.id"
       class="mr-2">
-    <img :src="item.picture"
-      :alt="item.title"
+    <img :src="itemCopy.picture"
+      :alt="itemCopy.title"
       class="w-16 h-16 object-cover mr-4 mb-2 sm:mb-0 sm:mr-8">
     <div>
-      <h2 class="text-lg font-bold">{{ item.title }}</h2>
-      <p class="text-gray-500">單價: {{ item.price }}</p>
-      <p class="text-gray-500">數量: {{ item.quantity }}</p>
+      <h2 class="text-lg font-bold">{{ itemCopy.title }}</h2>
+      <p class="text-gray-500">單價: {{ itemCopy.price }}</p>
+      <p class="text-gray-500">數量: {{ itemCopy.quantity }}</p>
     </div>
     <button @click="$emit('remove')"
       class="ml-auto text-red-500 hidden sm:block">
@@ -22,12 +22,20 @@
 <script>
 export default {
   props: ['item'],
+  data() {
+    return {
+      itemCopy: null,
+    };
+  },
   computed: {
     isMobile() {
       // 根據您的需求，這裡可以添加檢測設備是移動設備還是桌面設備的邏輯
       // 例如使用window.innerWidth來判斷屏幕寬度
       return true; // 假設都是移動設備
     },
+  },
+  created() {
+    this.itemCopy = { ...this.item }; // 創建一個淺拷貝的副本
   },
 };
 </script>
